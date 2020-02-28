78cd3721ff470890a22528addd4fc4fd78e7f0ba
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 13:25:38 +0100

    EOD

--- example_7.R
+++ example_7.R
@@ -6,16 +6,19 @@ library(beautier)
 # Constants
 ################################################################################
 is_testing <- is_on_travis()
-
-root_folder <- getwd()
 example_no <- 7
 rng_seed <- 314
-folder_name <- file.path(root_folder, paste0("example_", example_no, "_", rng_seed))
+folder_name <- paste0("example_", example_no, "_", rng_seed)
 
-
+################################################################################
+# Create phylogeny
+################################################################################
 set.seed(rng_seed)
 phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
 
+################################################################################
+# Setup pirouette
+################################################################################
 pir_params <- create_std_pir_params(folder_name = folder_name)
 # Remove candidates
 pir_params$experiments <- pir_params$experiments[1]
